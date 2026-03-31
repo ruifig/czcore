6dbb33c018202ab10ab914f7807cd0dc8b65e34b
ruifig <ruimvfigueira@gmail.com>
2026-03-31 17:10:53 +0100

    Disabled Catch2 warning(s)

--- UnitTests/CoreUnitTestsPch.h
+++ UnitTests/CoreUnitTestsPch.h
@@ -3,7 +3,12 @@
 #include "crazygaze/core/Common_Macros.h"
 
 CZ_THIRD_PARTY_INCLUDES_START
+
+__pragma(warning(push))
+__pragma(warning(disable: 5267))  /* definition of implicit copy constructor/assignment operator for 'type' is deprecated because it has a user-provided assignment operator/copy constructor */
 #include <catch2/catch_all.hpp>
+__pragma(warning(pop))
+
 //#include <catch2/catch_session.hpp>
 //#include <catch2/reporters/catch_reporter_registrars.hpp>
 CZ_THIRD_PARTY_INCLUDES_END
